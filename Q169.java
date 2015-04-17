412dd7ddf82a371a65f4ef013dd3c64187d5da57
jaurora <zhjie@umich.edu>
2015-04-16 20:40:45 -0400

    Q169

--- Q169.java
+++ Q169.java
@@ -2,12 +2,11 @@ import java.util.*;
 
 public class Solution {
     public int majorityElement(int[] num) {
-        
+        Arrays.sort(num);
+        return num[num.length/2];
     }
 
    public static void main(String[] args) {
        Solution slu = new Solution();
-       String s = "AB";
-       StdOut.println(slu.titleToNumber(s));
    }
 }
