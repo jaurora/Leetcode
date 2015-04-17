3220480f318fd155de0d13916065e66edb7a76d0
jaurora <zhjie@umich.edu>
2015-04-16 20:40:00 -0400

    Q169

---
+++ Q169.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    public int majorityElement(int[] num) {
+        
+    }
+
+   public static void main(String[] args) {
+       Solution slu = new Solution();
+       String s = "AB";
+       StdOut.println(slu.titleToNumber(s));
+   }
+}
