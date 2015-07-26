30eb12e6e22bbfa1cf29fab60424aff1e1efd151
jaurora <zhjie@umich.edu>
2015-07-25 23:26:54 -0400

    Q129

--- Q129.java
+++ Q129.java
@@ -12,8 +12,9 @@ import java.lang.*;
  */
 
 public class Solution {
-    List<Integer> ls = new ArrayList<Integer>();
+    private List<Integer> ls = new ArrayList<Integer>();
     public int sumNumbers(TreeNode root) {
+        if (root == null) return 0;
         int sum = 0;
         addnode(root, 0);
         for (int i=0; i<ls.size(); i++) sum += ((Integer) ls.get(i)).intValue();
