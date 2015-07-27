f67d0032507b89daad1c24cb951ccb2056c915f5
jaurora <zhjie@umich.edu>
2015-07-27 13:22:01 -0400

    Q104

---
+++ Q104.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        
+    }
+
+   public static void main(String[] args) {
+       Solution slu = new Solution();
+       TreeNode a = TreeNode()
+       //ListNode ans = slu.reverseBetween(a, 2,4);
+       //while (ans != null) { System.out.println(ans.val); ans = ans.next; }
+   }
+}
+
+
