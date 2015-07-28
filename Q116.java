5c38b51b961c2a4c7601b9d93d37a47d7d0cf7cc
jaurora <zhjie@umich.edu>
2015-07-27 23:01:09 -0400

    Q116

--- Q116.java
+++ Q116.java
@@ -13,17 +13,17 @@ import java.lang.*;
 public class Solution {
     public void connect(TreeLinkNode root) {
         if (root == null) return;
-        List<TreeListNode> l = new ArrayList<TreeListNode>();
+        List<TreeLinkNode> l = new ArrayList<TreeLinkNode>();
         l.add(root);
         level(l);
     }
 
     private void level(List<TreeLinkNode> lt) {
         if (lt.size() < 1) return;
-        List<TreeLinkNode> tmp = new ArrayList<Integer>();
+        List<TreeLinkNode> tmp = new ArrayList<TreeLinkNode>();
         for (int i=0; i<lt.size(); i++) {
-            TreeListNode p = (TreeListNode) lt.get(i);
-            if (i < lt.size()-1) lt.get(i).next = (TreeListNode) lt.get(i+1);
+            TreeLinkNode p = (TreeLinkNode) lt.get(i);
+            if (i < lt.size()-1) lt.get(i).next = (TreeLinkNode) lt.get(i+1);
             else lt.get(i).next = null;
             if (p.left != null) tmp.add(p.left);
             if (p.right != null) tmp.add(p.right);
